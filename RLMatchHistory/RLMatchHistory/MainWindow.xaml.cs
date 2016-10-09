7e4ab9d905c4891dbbed77bb62083ca880f64568
James Quigley <james@quigley.us>
2016-10-08 19:27:51 -0700

    Setup VB with needed libs

---
+++ RLMatchHistory/RLMatchHistory/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using RocketLeagueReplayParser;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace RLMatchHistory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
