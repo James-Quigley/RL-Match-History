2977ba2236ab8242540a856f24c2511e4af6bd23
James Quigley <james@quigley.us>
2016-10-10 10:50:40 -0700

    Actually posts game to the db

--- RLMatchHistoryWeb/routes/api.js
+++ RLMatchHistoryWeb/routes/api.js
@@ -18,21 +18,21 @@ router.get('/player/:playerID/game/:gameID', function(req, res, next) {
 
 router.post('/player/:playerID/game/:gameID', function(req, res, next) {
     var game = new Game({
-        TeamSize : req.body.Teamsize,
+        TeamSize : req.body.TeamSize,
         Team0Score : req.body.Team0Score,
         Team1Score : req.body.Team1Score,
         Goals : req.body.Goals,
         PlayerStats : req.body.PlayerStats,
         ReplayName : req.body.ReplayName,
-        GameId : req.body.GameId,
+        GameId : req.body.Id,
         MapName : req.body.MapName,
         Date : req.body.Date,
         MatchType : req.body.MatchType,
         PlayerID : req.body.PlayerName
     });
     
-    console.log(game.PlayerID);
-    
+    console.log(game);
+
     game.save(function(err){
         if (err)
             res.send(err);
