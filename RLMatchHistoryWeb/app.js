f776d280e3b6b9f5c6da35413ea5f5277d1efa55
James Quigley <james@quigley.us>
2016-10-10 10:33:58 -0700

    Now I actually resolved that conflict

--- RLMatchHistoryWeb/app.js
+++ RLMatchHistoryWeb/app.js
@@ -13,11 +13,25 @@ var mongoose = require('mongoose');
 
 require('./models/game');
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
 
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/api', api);
 
 
 // view engine setup
@@ -65,23 +79,6 @@ passport.use(new SteamStrategy({
     });
   }
 ));
-
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
-app.use(passport.initialize());
-app.use(passport.session());
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/api', api);
 
 app.get('/auth/steam',
   passport.authenticate('steam'),
