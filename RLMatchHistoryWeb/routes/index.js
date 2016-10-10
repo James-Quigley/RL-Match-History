0cc6080983bdd55f6f662240d8c3599eddd840ce
James Quigley <james@quigley.us>
2016-10-09 22:51:04 -0700

    Authenticating but not able to get user data

--- RLMatchHistoryWeb/routes/index.js
+++ RLMatchHistoryWeb/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Ayy lmao', user: req.user });
+    
 });
 
 module.exports = router;
